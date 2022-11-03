d5d9d4efeab8c77a2bac6149112a340b8e61496e
Filipe Ribeiro <68390743+FilipeRibeiroDev@users.noreply.github.com>
2022-11-03 07:58:27 -0300

    First Commit

---
+++ WebScraping/Program.cs
@@ -0,0 +1,18 @@
+﻿using EasyAutomationFramework;
+using EasyAutomationFramework.Model;
+using WebScraping.Driver;
+
+var web = new WebScraper();
+var computeres = web.GetData("https://www.webscraper.io/test-sites/e-commerce/allinone/computers");
+var laptops = web.GetData("https://www.webscraper.io/test-sites/e-commerce/allinone/computers/laptops");
+var tablets = web.GetData("https://www.webscraper.io/test-sites/e-commerce/allinone/computers/tablets");
+
+
+var paramss = new ParamsDataTable("Dados", @"C:\temp\excel", new List<DataTables>()
+{
+    new DataTables("computeres", computeres),
+    new DataTables("laptops", laptops),
+    new DataTables("tablets", tablets),
+});
+
+Base.GenerateExcel(paramss);
